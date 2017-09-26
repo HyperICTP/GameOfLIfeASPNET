4db3aa5f0e983daae7fc02c2c9fbcfb4cb1b3200
SoftwareEngineeringPractice2017 <31974523+SoftwareEngineeringPractice2017@users.noreply.github.com>
2017-09-26 12:37:08 +1000

    Add project files.

---
+++ GameOfLIfeASPNET/Controllers/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameOfLIfeASPNET.Controllers
+{
+    public class MyController
+    {
+        [HttpGet("test/greg")]
+        public string Greg()
+        {
+            return "HI!";
+        }
+
+        [HttpGet( "test/steve" )]
+        public string Steve()
+        {
+            return "Yo.";
+        }
+    }
+}
